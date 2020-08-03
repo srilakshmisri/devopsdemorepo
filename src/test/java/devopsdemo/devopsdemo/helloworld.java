6a0124a4e0a8ec41e69604d49372c5166390c104
srilakshmisri <35537638+srilakshmisri@users.noreply.github.com>
2020-08-03 11:37:05 +0530

    this is git code by sri devops demo

---
+++ src/test/java/devopsdemo/devopsdemo/helloworld.java
@@ -0,0 +1,12 @@
+package devopsdemo.devopsdemo;
+
+import org.testng.annotations.Test;
+
+public class helloworld {
+  @Test
+  public void f() {
+	  
+	  
+	  System.out.println("hello world... learning testing in devops");
+  }
+}
